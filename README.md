27573634c7d4a87d7a8c0cbd4af9e20491b66de6
Sonia <sonexkayjay@gmail.com>
2024-09-25 16:46:08 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# light_and_dark_mode
+
+A new Flutter project.
+# Light-dark-mode-Form
---
+++ lib/main.dart
@@ -0,0 +1,129 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+class MainApp extends StatefulWidget {
+  const MainApp({super.key});
+
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  bool _isDarkTheme = false; 
+  void _toggleTheme(){
+    setState(() {
+      _isDarkTheme = !_isDarkTheme;
+    
+    });
+  }
+  
+  @override
+
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title:'Theme Switcher',
+      theme: ThemeData(
+        primarySwatch: Colors.brown,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        brightness: Brightness.light,
+        
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      themeMode: _isDarkTheme? ThemeMode.dark: ThemeMode.light,
+      home:HomePage (toggleTheme: _toggleTheme),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  final VoidCallback toggleTheme;
+  const HomePage({required this.toggleTheme, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+       appBar: AppBar(
+        title: const Text('Theme Switcher'),
+        backgroundColor: const Color.fromARGB(255, 200, 159, 198),  
+        actions: [
+          IconButton(            
+          icon:const Icon(Icons.toggle_off_outlined),
+          onPressed: toggleTheme, 
+          )
+        ],     
+       ),
+       body: Center(   
+        child:  Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            const MyTextField(),
+            const SizedBox(height: 59,),
+             ElevatedButton(onPressed: (){
+
+             }, 
+             child: const Text('Submit me')),
+
+          ],
+        ),
+        
+        
+       ),
+    );
+  }
+}
+class MyTextField extends StatefulWidget {
+  const MyTextField({super.key});
+
+  @override
+  State<MyTextField> createState() => _MyTextFieldState();
+}
+
+class _MyTextFieldState extends State<MyTextField> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const SizedBox(height: 30,),
+        SizedBox(
+        width: 400,
+        child: TextFormField(
+          decoration: const InputDecoration(
+            labelText: 'Enter your name',
+            border: OutlineInputBorder(),
+
+          ),
+        ),
+        ),
+        const SizedBox(height: 15,),
+        SizedBox(
+        width: 400,
+        child: TextFormField(
+          decoration:const InputDecoration(
+            labelText: 'Enter your email  address',
+            border: OutlineInputBorder(),
+          ),
+
+        ),
+        ),
+        const SizedBox(height: 15,),
+        SizedBox(
+          width: 400,
+         child: TextFormField(
+          decoration:const InputDecoration(
+            labelText: 'Enter your password',
+            border: OutlineInputBorder(),
+      
+
+          ),
+        ),
+        ),
+      ],
+    );
+  }
+}
